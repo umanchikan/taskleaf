2c5d4080f16286ee4c42d98451ad5a3974958626
chiakisaito <a.cop.p2p2@gmail.com>
2020-08-12 06:07:46 +0900

    Add: statusカラムを追加。Tasksテーブルに

---
+++ db/migrate/20200811210411_add_cullum_status_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddCullumStatusToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :status, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_200152) do
+ActiveRecord::Schema.define(version: 2020_08_11_210411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_200152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "expired_at", default: "2020-09-11 20:05:12"
+    t.integer "status"
   end
 
 end
