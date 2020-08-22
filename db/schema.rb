ad7f4d914c1e6e59e1b060e779994010c50ca188
chiakisaito <a.cop.p2p2@gmail.com>
2020-08-22 18:42:55 +0900

    ADD title,statusカラムにインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_210411) do
+ActiveRecord::Schema.define(version: 2020_08_22_094017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_210411) do
     t.datetime "updated_at", null: false
     t.datetime "expired_at", default: "2020-09-11 20:05:12"
     t.integer "status"
+    t.index ["title", "status"], name: "index_tasks_on_title_and_status"
   end
 
 end
